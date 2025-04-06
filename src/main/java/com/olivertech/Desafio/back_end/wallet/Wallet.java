12c12ee171437fcd36b2fb738ad92e230153954c
OliverBR1 <Br.0liveira@outlook.com>
2025-04-06 00:20:18 -0300

    Update wallet

--- src/main/java/com/olivertech/Desafio/back_end/wallet/Wallet.java
+++ src/main/java/com/olivertech/Desafio/back_end/wallet/Wallet.java
@@ -1,9 +1,11 @@
 package com.olivertech.Desafio.back_end.wallet;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 
+@Table("WALLETS")
 public record Wallet(
     @Id Long id,
     String fullname,
