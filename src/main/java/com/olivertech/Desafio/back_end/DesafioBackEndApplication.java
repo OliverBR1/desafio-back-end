352df56a856b906b3c94cdb68a8e811609fc0532
OliverBR1 <Br.0liveira@outlook.com>
2025-04-06 00:18:46 -0300

    Update main

--- src/main/java/com/olivertech/Desafio/back_end/DesafioBackEndApplication.java
+++ src/main/java/com/olivertech/Desafio/back_end/DesafioBackEndApplication.java
@@ -1,8 +1,11 @@
 package com.olivertech.Desafio.back_end;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+import org.springframework.kafka.config.TopicBuilder;
 
 @EnableJdbcAuditing
 @SpringBootApplication
@@ -10,6 +13,12 @@ public class DesafioBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioBackEndApplication.class, args);
+	}
+
+	@Bean
+	NewTopic notificationTopic() {
+		return TopicBuilder.name("transaction-notification")
+				.build();
 	}
 
 }
